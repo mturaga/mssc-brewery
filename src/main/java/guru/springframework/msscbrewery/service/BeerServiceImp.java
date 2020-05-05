c9e87786ab28629fbddd955578967c167b966174
mturaga <maruthi.turaga@gmail.com>
2020-05-04 19:11:25 -0500

    initial

--- src/main/java/guru/springframework/msscbrewery/service/BeerServiceImp.java
+++ src/main/java/guru/springframework/msscbrewery/service/BeerServiceImp.java
@@ -1,4 +1,17 @@
 package guru.springframework.msscbrewery.service;
 
-public class BeerServiceImp {
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceImp implements BeerService {
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder().id(beerId)
+                .beerName("India Pale Ale")
+                .beerStyle("Pale Ale")
+                .build();
+    }
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -1,4 +1,29 @@
 package guru.springframework.msscbrewery.web.controller;
 
+import guru.springframework.msscbrewery.service.BeerService;
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
 public class BeerController {
+
+    private final BeerService beerService;
+
+    public BeerController(BeerService service){
+        this.beerService = service;
+    }
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeer( @PathVariable("beerId") UUID beerId){
+        return new ResponseEntity<BeerDto>(this.beerService.getBeerById(beerId), HttpStatus.OK );
+    }
+
 }
